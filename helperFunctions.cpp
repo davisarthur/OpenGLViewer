4096f5e0063052c6144efc601f7cfa55045a7715
Davis Arthur <dsa0013@auburn.edu>
2021-10-29 21:41:12 -0500

    updates

--- helperFunctions.cpp
+++ helperFunctions.cpp
@@ -262,7 +262,7 @@ void loadRose(SceneObject& model, glm::vec3& eye, float& znear, float& zfar, Dir
 
 void loadHead(SceneObject& model, glm::vec3& eye, float& znear, float& zfar, DirectionalLight& lightSource) {
    model = SceneObject("data/head.obj", glm::vec3(0.0, 0.0, 0.0), glm::vec3(1.0, 1.0, 1.0), glm::vec3(3.0 * M_PI / 2.0 * 0.92, 0.0, 0.0));
-   eye = glm::vec3(0.0, 4.0, 20.0);
+   eye = glm::vec3(0.0, 1.6, 8.0);
    lightSource.intensity = 1.0f;
    lightSource.dir = glm::vec3(0.5, 1.0, 1.0);
    znear = 1.0f;
